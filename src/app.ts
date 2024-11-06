7777f072da47181f3f75ee9b44d3f4f4c69b5255
devjuanmarcos <jncontatojn@gmail.com>
2024-11-06 00:45:28 -0300

    feat: swagger configurado

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,26 @@
 import express, { Application } from "express";
 import { jsonMiddleware } from "./middlewares/jsonMiddleware";
 import router from "./routes/translationRoutes";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
 const app: Application = express();
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: "Kikito API",
+      version: "1.0.0",
+      description: "A melhor e mais bonita API em Node.js",
+    },
+    basePath: "/",
+  },
+  apis: ["./src/routes/*.ts"],
+};
+
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(jsonMiddleware);
 app.use("/api", router);
--- src/routes/translationRoutes.ts
+++ src/routes/translationRoutes.ts
@@ -10,3 +10,69 @@ router.post("/translate-json", translateJsonEndpoint);
 router.post("/translate-all-languages", translateFullLanguagesEndpoint);
 
 export default router;
+
+/**
+ * @swagger
+ * /translate-json:
+ *   post:
+ *     summary: "Translate a JSON"
+ *     description: "Translate the content of a JSON object into a specified language."
+ *     parameters:
+ *       - name: "language"
+ *         in: query
+ *         description: "The language to translate to"
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             additionalProperties:
+ *               type: string
+ *     responses:
+ *       200:
+ *         description: "Successfully translated the JSON."
+ *       400:
+ *         description: "Bad Request. Missing required parameters."
+ *       500:
+ *         description: "Internal Server Error."
+ */
+
+/**
+ * @swagger
+ * /translate-all-languages:
+ *   post:
+ *     summary: "Translate JSON content to all supported languages"
+ *     description: "Translate the content of a JSON object to all languages supported by the service."
+ *     parameters:
+ *       - name: "language"
+ *         in: query
+ *         description: "The language to start the translation process."
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             additionalProperties:
+ *               type: string
+ *     responses:
+ *       200:
+ *         description: "Successfully translated the JSON to all languages."
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               additionalProperties:
+ *                 type: string
+ *       400:
+ *         description: "Bad Request. Missing required parameters."
+ *       500:
+ *         description: "Internal Server Error. An unexpected error occurred during the translation."
+ */
