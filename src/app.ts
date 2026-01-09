c18abb93824656b6d097f05d8c407f934b69e88f
Juan Marcos <jncontatojn@gmail.com>
2026-01-09 16:56:23 -0300

    Add translated JSON files for multiple languages including Tamil, Telugu, Thai, Turkish, Ukrainian, Urdu, Vietnamese, Yoruba, Cantonese, Simplified and Traditional Chinese.

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,15 @@ import swaggerJSDoc from "swagger-jsdoc";
 
 const app: Application = express();
 
+// Definindo um tempo de timeout para todas as requisições (em milissegundos)
+app.use((req, res, next) => {
+  res.setTimeout(300000, () => {
+    // 300000 ms = 5 minutos
+    console.log("Requisição demorou mais de 5 minutos.");
+  });
+  next();
+});
+
 const swaggerOptions = {
   definition: {
     openapi: "3.0.0",
